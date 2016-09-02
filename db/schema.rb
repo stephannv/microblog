12c8f07157dd8736168105eeebde28d44a690002
_stephann <stephannvasconcelos@gmail.com>
2016-09-02 19:51:22 -0300

    Criando modelo post #2

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160902011838) do
+ActiveRecord::Schema.define(version: 20160902224023) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "posts", force: :cascade do |t|
+    t.integer  "user_id"
+    t.string   "text",       limit: 256, null: false
+    t.datetime "created_at",             null: false
+    t.datetime "updated_at",             null: false
+    t.index ["user_id"], name: "index_posts_on_user_id", using: :btree
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "username",            limit: 12,               null: false
@@ -33,4 +41,5 @@ ActiveRecord::Schema.define(version: 20160902011838) do
     t.index ["username"], name: "index_users_on_username", unique: true, using: :btree
   end
 
+  add_foreign_key "posts", "users"
 end
